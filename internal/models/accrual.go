a6eccba60faa4b8dfe91a8f12399fac3310f8c17
vorotislav <azovskiyv@gmail.com>
2023-11-29 10:38:13 +0500

    update model of order accrual

--- internal/models/accrual.go
+++ internal/models/accrual.go
@@ -1,7 +1,7 @@
 package models
 
 type OrderAccrual struct {
-	Order   string `json:"order"`
-	Status  string `json:"status"`
-	Accrual int    `json:"accrual"`
+	Order   string  `json:"order"`
+	Status  string  `json:"status"`
+	Accrual float32 `json:"accrual"`
 }
